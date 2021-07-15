4c1080740627175f43dcc8cbcf920ad8f30334ee
Anwar Ali Khan <connectakonline@gmail.com>
2021-07-14 22:00:38 -0400

    Added more attributes from cucumber json

--- src/app/components/dashboard/cucumber-json-parser/cucumber-json-parser.component.ts
+++ src/app/components/dashboard/cucumber-json-parser/cucumber-json-parser.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { AfterViewInit, ElementRef} from '@angular/core';
+import { CucumberReport } from 'src/app/model/CucumberReport';
+
 
 
 @Component({
@@ -8,17 +11,13 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./cucumber-json-parser.component.css']
 })
 export class CucumberJsonParserComponent implements OnInit {
-
   jsonPath: string = 'assets/data/cucumber/sample.json';
   features: any = [];
-
-  
   constructor(private httpClient: HttpClient){}
   ngOnInit(){
     this.httpClient.get(this.jsonPath).subscribe(data =>{
       //console.log(data);
-      this.features = data;
+      this.features = data;  
     })
   }
-
 }
---
+++ src/app/model/CucumberReport.ts
@@ -0,0 +1,10 @@
+export interface  CucumberReport {
+
+    id : string;
+    name : string;
+    description : string;
+    line : number;
+    keyword : string;
+    uri : string;
+
+}
