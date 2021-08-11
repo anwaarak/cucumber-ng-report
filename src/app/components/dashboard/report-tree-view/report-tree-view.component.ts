6ae2e2334824eccd9c5ab24e8dbf747c889cf45f
Urvashi <dubeyurvashi8853@gmail.com>
2021-08-11 20:33:58 +0530

    added tree view

--- src/app/components/dashboard/report-tree-view/report-tree-view.component.ts
+++ src/app/components/dashboard/report-tree-view/report-tree-view.component.ts
@@ -1,130 +1,226 @@
-import { Component, OnInit } from '@angular/core';
-import {NestedTreeControl} from '@angular/cdk/tree';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import {
+  MatTreeFlattener,
+  MatTreeFlatDataSource
+} from '@angular/material/tree';
+import { of as ofObservable, Observable, BehaviorSubject } from 'rxjs';
 
-interface Description {
-    description: string;
-    elements: Elements[];
-  }
-
-  interface Elements {
-    description: string;
-    steps: Step[];
-  }
-
-  interface Step {
-    duration: number;
-    status: string;
-  }
-
-  interface TreeNode {
-    name: string;
-    children?: TreeNode[];
-  }
-
-function abc(){
-  return <JSON><unknown> {
-        "description": "This is description of the feature",
-        "elements": [
-            {
-                "description": "Perfect background",
-                "steps": [
-                    {
-                        "result": {
-                            "duration": 99107447000,
-                            "status": "passed"
-                        }
-                    }
-                  ]
-            }
-          ]
-  }
+/**
+ * Node for report  item
+ */
+export class ReportNode {
+  children: ReportNode[];
+  item: string;
 }
 
-//Receive raw json and construct tree
-  function constructTree(json: JSON) {
-    let parsedJson: Description = JSON.parse(JSON.stringify(abc()));
-   // console.log((parsedJson[0].elements[0].description))
-    //for(let i = 0; i < parsedJson.length; ++i) {
-      TREE_DATA.push(parseDescription(parsedJson));
-   // }
-  }
+/** Flat report item node with expandable and level information */
+export class ReportFlatNode {
+  item: string;
+  level: number;
+  expandable: boolean;
+}
 
-  //Parse innermost child(Steps)
-  function parseSteps(s : Step[]) {
-    console.log("parsed steps")
-    let parent: TreeNode;
-    parent.name = "Steps"
-    for(let i = 0; i < s.length; ++i) {
-      let tmp1: TreeNode;
-      let tmp2: TreeNode;
-      tmp1.name = <string><unknown>s[0].duration;
-      tmp2.name = s[0].status;
-      this.parent.children.push(tmp1);
-      this.parent.children.push(tmp2);
+/**
+ * The Json object for report  data.
+ */
+const TREE_DATA = {
+  description: 'This is description of the feature',
+  elements: [
+    {
+      description: 'Perfect background',
+      steps: [
+        {
+          result: {
+            duration: 99107447000,
+            status: 'passed'
+          }
+        }
+      ]
     }
-    return parent;
+  ]
+};
+
+/**
+ * Checklist database, it can build a tree structured Json object.
+ * Each node in Json object represents a report item or a category.
+ * If a node is a category, it has children items and new items can be added under the category.
+ */
+@Injectable()
+export class ChecklistDatabase {
+  dataChange: BehaviorSubject<ReportNode[]> = new BehaviorSubject<
+    ReportNode[]
+  >([]);
+
+  get data(): ReportNode[] {
+    return this.dataChange.value;
   }
 
-  //Parse inner child (Elements)
-  function parseElements(e : Elements[]) {
-      let parent: TreeNode;
-      parent.name = "Elements"
-      for(let i = 0; i < e.length; ++i) {
-        let tmp: TreeNode;
-        tmp.children.push(parseSteps(e[i].steps));
-        this.parent.children.push(tmp);
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Build the tree nodes from Json object. The result is a list of `ReportmNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(TREE_DATA, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `ReportNode`.
+   */
+  buildFileTree(value: any, level: number) {
+    let data: any[] = [];
+    for (let k in value) {
+      let v = value[k];
+      let node = new ReportNode();
+      node.item = `${k}`;
+      if (v === null || v === undefined) {
+        // no action
+      } else if (typeof v === 'object') {
+        node.children = this.buildFileTree(v, level + 1);
+      } else {
+        node.item = v;
+      }
+      data.push(node);
     }
-    return parent;
+    return data;
   }
 
-  //Parse parent (description)
-  function parseDescription(d: Description) {
-    let parent: TreeNode;
-    //console.log(d.description.elements[0].description)
-    this.parent.name = d.description;
-    this.parent.children = parseElements(<Elements[]>d.elements);
-    return parent;
-  }
+}
 
-let TREE_DATA: TreeNode[];
-
+/**
+ * @title Tree with checkboxes
+ */
 @Component({
   selector: 'app-report-tree-view',
   templateUrl: './report-tree-view.component.html',
-  styleUrls: ['./report-tree-view.component.css']
+  styleUrls: ['./report-tree-view.component.css'],
+   providers: [ChecklistDatabase]
 })
 
 export class ReportTreeViewComponent implements OnInit {
 
-treeControl = new NestedTreeControl<TreeNode>(node => node.children);
-  dataSource = new MatTreeNestedDataSource<TreeNode>();
+  /** Map from flat node to nested node. This helps us finding the nested node to be modified */
+  flatNodeMap: Map<ReportFlatNode, ReportNode> = new Map<
+    ReportFlatNode,
+    ReportNode
+  >();
 
-  constructor() {
-    constructTree(null)
-    this.dataSource.data = TREE_DATA;
+  /** Map from nested node to flattened node. This helps us to keep the same object for selection */
+  nestedNodeMap: Map<ReportNode, ReportFlatNode> = new Map<
+    ReportNode,
+    ReportFlatNode
+  >();
+
+  /** A selected parent node to be inserted */
+  selectedParent: ReportFlatNode | null = null;
+
+  /** The new item's name */
+  newItemName: string = '';
+
+  treeControl: FlatTreeControl<ReportFlatNode>;
+
+  treeFlattener: MatTreeFlattener<ReportNode, ReportFlatNode>;
+
+  dataSource: MatTreeFlatDataSource<ReportNode, ReportFlatNode>;
+
+  /** The selection for checklist */
+  checklistSelection = new SelectionModel<ReportFlatNode>(
+    true /* multiple */
+  );
+
+  constructor(private database: ChecklistDatabase) {
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer,
+      this.getLevel,
+      this.isExpandable,
+      this.getChildren
+    );
+    this.treeControl = new FlatTreeControl<ReportFlatNode>(
+      this.getLevel,
+      this.isExpandable
+    );
+    this.dataSource = new MatTreeFlatDataSource(
+      this.treeControl,
+      this.treeFlattener
+    );
+
+    database.dataChange.subscribe(data => {
+      this.dataSource.data = data;
+    });
   }
 
-  ngOnInit(): void {
+  getLevel = (node: ReportFlatNode) => {
+    return node.level;
+  };
+
+  isExpandable = (node: ReportFlatNode) => {
+    return node.expandable;
+  };
+
+  getChildren = (node: ReportNode): Observable<ReportNode[]> => {
+    return ofObservable(node.children);
+  };
+
+  hasChild = (_: number, _nodeData: ReportFlatNode) => {
+    return _nodeData.expandable;
+  };
+
+  hasNoContent = (_: number, _nodeData: ReportFlatNode) => {
+    return _nodeData.item === '';
+  };
+
+  /**
+   * Transformer to convert nested node to flat node. Record the nodes in maps for later use.
+   */
+  transformer = (node: ReportNode, level: number) => {
+    let flatNode =
+      this.nestedNodeMap.has(node) &&
+      this.nestedNodeMap.get(node)!.item === node.item
+        ? this.nestedNodeMap.get(node)!
+        : new ReportFlatNode();
+    flatNode.item = node.item;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children;
+    this.flatNodeMap.set(flatNode, node);
+    this.nestedNodeMap.set(node, flatNode);
+    return flatNode;
+  };
+
+  /** Whether all the descendants of the node are selected */
+  descendantsAllSelected(node: ReportFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    return descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
   }
 
-  hasChild = (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+  /** Whether part of the descendants are selected */
+  descendantsPartiallySelected(node: ReportFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    const result = descendants.some(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    return result && !this.descendantsAllSelected(node);
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  /** Toggle the to-do item selection. Select/deselect all the descendants node */
+  todoItemSelectionToggle(node: ReportFlatNode): void {
+    this.checklistSelection.toggle(node);
+    const descendants = this.treeControl.getDescendants(node);
+    this.checklistSelection.isSelected(node)
+      ? this.checklistSelection.select(...descendants)
+      : this.checklistSelection.deselect(...descendants);
+  }
+   ngOnInit(): void {
+  }
+}
 
 
 /*
